76f690b7ed156da6583e0ff02364fd8902d694d6
Simão Costa <simadinisvc@gmail.com>
2022-04-21 09:50:45 +0100

    F and G Done

--- Training/F T22.py
+++ Training/F T22.py
@@ -3,14 +3,18 @@ Ni = list(map(int, input().split()))
 M = int(input())
 Mi = list(map(int, input().split()))
 
-pares = 0
-impares = 0
+i1 = 0
+for i in Ni:
+    if i & 1:
+        i1 += 1
+p1 = N - i1
 
-for n in Ni:
-    for m in Mi:
-        if (n%2 == m%2):
-            pares += 1
-        else:
-            impares += 1
+i2 = 0
+for i in Mi:
+    if i & 1:
+        i2+=1
+p2 = M - i2
 
-print(pares, impares)
+pares = (p1*p2) + (i1*i2)
+
+print(pares, (N*M) - pares)
